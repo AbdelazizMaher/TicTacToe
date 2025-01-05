a72c966567faba772a6b0675dc8d3affe28ab31d
Adham Mohamed <adhambiko22@gmail.com>
2025-01-05 16:20:01 +0200

    Modify Home Page Labels

--- ClientApp/src/XOGame/HomePage.java
+++ ClientApp/src/XOGame/HomePage.java
@@ -1,6 +1,5 @@
 package XOGame;
 
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
@@ -9,30 +8,30 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
-import javafx.stage.Stage;
 
 public abstract class HomePage extends AnchorPane {
 
-    protected final AnchorPane anchorPane;
     protected final ImageView imageView;
+    protected final AnchorPane anchorPane;
     protected final ImageView imageView0;
+    protected final ImageView imageView1;
     protected final Line line;
     protected final Line line0;
     protected final Line line1;
     protected final Line line2;
     protected final Label tictactoeLabel;
-    protected final ImageView imageView1;
+    protected final ImageView imageView2;
     protected final Label welcometoourtictactoeLabel;
     protected final Button historyButton;
     protected final Button signupButton;
     protected final Button loginButton;
     protected final Button playvscomputerButton;
-    protected final ImageView imageView2;
-    protected final Button playofflineButton;
     protected final ImageView imageView3;
-    protected final Button playonlineButton;
+    protected final Button playofflineButton;
     protected final ImageView imageView4;
+    protected final Button playonlineButton;
     protected final ImageView imageView5;
+    protected final ImageView imageView6;
     protected final Label label;
     protected final DropShadow dropShadow;
     protected final Label label0;
@@ -42,26 +41,27 @@ public abstract class HomePage extends AnchorPane {
 
     public HomePage() {
 
-        anchorPane = new AnchorPane();
         imageView = new ImageView();
+        anchorPane = new AnchorPane();
         imageView0 = new ImageView();
+        imageView1 = new ImageView();
         line = new Line();
         line0 = new Line();
         line1 = new Line();
         line2 = new Line();
         tictactoeLabel = new Label();
-        imageView1 = new ImageView();
+        imageView2 = new ImageView();
         welcometoourtictactoeLabel = new Label();
         historyButton = new Button();
         signupButton = new Button();
         loginButton = new Button();
         playvscomputerButton = new Button();
-        imageView2 = new ImageView();
-        playofflineButton = new Button();
         imageView3 = new ImageView();
-        playonlineButton = new Button();
+        playofflineButton = new Button();
         imageView4 = new ImageView();
+        playonlineButton = new Button();
         imageView5 = new ImageView();
+        imageView6 = new ImageView();
         label = new Label();
         dropShadow = new DropShadow();
         label0 = new Label();
@@ -75,26 +75,30 @@ public abstract class HomePage extends AnchorPane {
         getStyleClass().add("bodybg");
         getStylesheets().add("/styles/Stylesheet.css");
 
+        imageView.setFitHeight(580.0);
+        imageView.setFitWidth(780.0);
+        imageView.setPreserveRatio(false);
+
         anchorPane.setPrefHeight(580.0);
         anchorPane.setPrefWidth(80.0);
         anchorPane.getStyleClass().add("sidebar");
         anchorPane.getStylesheets().add("/styles/Stylesheet.css");
 
-        imageView.setFitHeight(50.0);
-        imageView.setFitWidth(55.0);
-        imageView.setLayoutX(15.0);
-        imageView.setLayoutY(9.0);
-        imageView.setPickOnBounds(true);
-        imageView.setPreserveRatio(true);
-        imageView.setImage(new Image(getClass().getResource("/media/xo.jpg").toExternalForm()));
-
-        imageView0.setFitHeight(70.0);
-        imageView0.setFitWidth(70.0);
-        imageView0.setLayoutX(5.0);
-        imageView0.setLayoutY(526.0);
+        imageView0.setFitHeight(50.0);
+        imageView0.setFitWidth(55.0);
+        imageView0.setLayoutX(15.0);
+        imageView0.setLayoutY(9.0);
         imageView0.setPickOnBounds(true);
         imageView0.setPreserveRatio(true);
-        imageView0.setImage(new Image(getClass().getResource("/media/avatar.png").toExternalForm()));
+        imageView0.setImage(new Image(getClass().getResource("/media/xo.jpg").toExternalForm()));
+
+        imageView1.setFitHeight(70.0);
+        imageView1.setFitWidth(70.0);
+        imageView1.setLayoutX(5.0);
+        imageView1.setLayoutY(526.0);
+        imageView1.setPickOnBounds(true);
+        imageView1.setPreserveRatio(true);
+        imageView1.setImage(new Image(getClass().getResource("/media/avatar.png").toExternalForm()));
 
         line.setEndX(-39.09999084472656);
         line.setEndY(-7.62939453125E-6);
@@ -129,21 +133,20 @@ public abstract class HomePage extends AnchorPane {
         line2.setStroke(javafx.scene.paint.Color.WHITE);
 
         tictactoeLabel.setLayoutX(91.0);
-        tictactoeLabel.setLayoutY(3.0);
         tictactoeLabel.setText("TicTacToe");
         tictactoeLabel.setTextFill(javafx.scene.paint.Color.valueOf("#131218"));
         tictactoeLabel.setFont(new Font("Javanese Text", 18.0));
 
-        imageView1.setFitHeight(75.0);
-        imageView1.setFitWidth(50.0);
-        imageView1.setLayoutX(729.0);
-        imageView1.setLayoutY(12.0);
-        imageView1.setPickOnBounds(true);
-        imageView1.setPreserveRatio(true);
-        imageView1.setImage(new Image(getClass().getResource("/media/avatar.png").toExternalForm()));
+        imageView2.setFitHeight(75.0);
+        imageView2.setFitWidth(50.0);
+        imageView2.setLayoutX(732.0);
+        imageView2.setLayoutY(12.0);
+        imageView2.setPickOnBounds(true);
+        imageView2.setPreserveRatio(true);
+        imageView2.setImage(new Image(getClass().getResource("/media/avatar.png").toExternalForm()));
 
         welcometoourtictactoeLabel.setLayoutX(91.0);
-        welcometoourtictactoeLabel.setLayoutY(43.0);
+        welcometoourtictactoeLabel.setLayoutY(41.0);
         welcometoourtictactoeLabel.setText("WELCOME TO OUR TICTACTOE GAME ..!");
         welcometoourtictactoeLabel.setTextFill(javafx.scene.paint.Color.valueOf("#131218"));
         welcometoourtictactoeLabel.setFont(new Font("Arial Bold", 16.0));
@@ -156,7 +159,7 @@ public abstract class HomePage extends AnchorPane {
         historyButton.setStyle("-fx-background-color: #e61409;");
         historyButton.setText("History");
         historyButton.setTextFill(javafx.scene.paint.Color.WHITE);
-        historyButton.setFont(new Font("Arial", 12.0));
+        historyButton.setFont(new Font("Arial Bold", 12.0));
 
         signupButton.setLayoutX(633.0);
         signupButton.setLayoutY(15.0);
@@ -166,7 +169,7 @@ public abstract class HomePage extends AnchorPane {
         signupButton.setStyle("-fx-background-color: #e61409;");
         signupButton.setText("Sign Up");
         signupButton.setTextFill(javafx.scene.paint.Color.WHITE);
-        signupButton.setFont(new Font("Arial", 12.0));
+        signupButton.setFont(new Font("Arial Bold", 12.0));
 
         AnchorPane.setLeftAnchor(loginButton, 691.0);
         loginButton.setLayoutX(691.0);
@@ -177,7 +180,7 @@ public abstract class HomePage extends AnchorPane {
         loginButton.setStyle("-fx-background-color: #e61409;");
         loginButton.setText("Login");
         loginButton.setTextFill(javafx.scene.paint.Color.WHITE);
-        loginButton.setFont(new Font("Arial", 12.0));
+        loginButton.setFont(new Font("Arial Bold", 12.0));
 
         playvscomputerButton.setLayoutX(265.0);
         playvscomputerButton.setLayoutY(219.0);
@@ -189,14 +192,12 @@ public abstract class HomePage extends AnchorPane {
         playvscomputerButton.setTextFill(javafx.scene.paint.Color.WHITE);
         playvscomputerButton.setFont(new Font("Arial Bold", 14.0));
 
-        imageView2.setFitHeight(100.0);
-        imageView2.setFitWidth(200.0);
-        imageView2.setPickOnBounds(true);
-        imageView2.setImage(new Image(getClass().getResource("/media/playvscomputer.jpg").toExternalForm()));
-        playvscomputerButton.setGraphic(imageView2);
-        
-        
-        
+        imageView3.setFitHeight(100.0);
+        imageView3.setFitWidth(200.0);
+        imageView3.setPickOnBounds(true);
+        imageView3.setImage(new Image(getClass().getResource("/media/playvscomputer.jpg").toExternalForm()));
+        playvscomputerButton.setGraphic(imageView3);
+
         playofflineButton.setLayoutX(265.0);
         playofflineButton.setLayoutY(338.0);
         playofflineButton.setMnemonicParsing(false);
@@ -206,13 +207,12 @@ public abstract class HomePage extends AnchorPane {
         playofflineButton.setTextAlignment(javafx.scene.text.TextAlignment.CENTER);
         playofflineButton.setTextFill(javafx.scene.paint.Color.WHITE);
         playofflineButton.setFont(new Font("Arial Bold", 14.0));
-        
-        
-        imageView3.setFitHeight(100.0);
-        imageView3.setFitWidth(215.0);
-        imageView3.setPickOnBounds(true);
-        imageView3.setImage(new Image(getClass().getResource("/media/playoffline.jpg").toExternalForm()));
-        playofflineButton.setGraphic(imageView3);
+
+        imageView4.setFitHeight(100.0);
+        imageView4.setFitWidth(215.0);
+        imageView4.setPickOnBounds(true);
+        imageView4.setImage(new Image(getClass().getResource("/media/playoffline.jpg").toExternalForm()));
+        playofflineButton.setGraphic(imageView4);
 
         playonlineButton.setLayoutX(266.0);
         playonlineButton.setLayoutY(457.0);
@@ -223,54 +223,54 @@ public abstract class HomePage extends AnchorPane {
         playonlineButton.setTextAlignment(javafx.scene.text.TextAlignment.CENTER);
         playonlineButton.setTextFill(javafx.scene.paint.Color.WHITE);
         playonlineButton.setFont(new Font("Arial Bold", 14.0));
-        
-        imageView4.setFitHeight(100.0);
-        imageView4.setFitWidth(185.0);
-        imageView4.setPickOnBounds(true);
-        imageView4.setImage(new Image(getClass().getResource("/media/playonline.jpg").toExternalForm()));
-        playonlineButton.setGraphic(imageView4);
 
-        imageView5.setFitHeight(400.0);
-        imageView5.setFitWidth(235.0);
-        imageView5.setLayoutX(544.0);
-        imageView5.setLayoutY(180.0);
+        imageView5.setFitHeight(100.0);
+        imageView5.setFitWidth(185.0);
         imageView5.setPickOnBounds(true);
-        imageView5.setImage(new Image(getClass().getResource("/media/o.jpg").toExternalForm()));
+        imageView5.setImage(new Image(getClass().getResource("/media/playonline.jpg").toExternalForm()));
+        playonlineButton.setGraphic(imageView5);
 
-        label.setLayoutX(360.0);
-        label.setLayoutY(312.0);
+        imageView6.setFitHeight(400.0);
+        imageView6.setFitWidth(235.0);
+        imageView6.setLayoutX(544.0);
+        imageView6.setLayoutY(180.0);
+        imageView6.setPickOnBounds(true);
+        imageView6.setImage(new Image(getClass().getResource("/media/o.jpg").toExternalForm()));
+
+        label.setLayoutX(270.0);
+        label.setLayoutY(224.0);
         label.setText("PLAY VS COMPUTER");
         label.setTextFill(javafx.scene.paint.Color.WHITE);
-        label.setFont(new Font("Arial Bold", 13.0));
-        
+        label.setFont(new Font("Arial Bold", 15.0));
 
         label.setEffect(dropShadow);
 
-        label0.setLayoutX(401.0);
-        label0.setLayoutY(431.0);
+        label0.setLayoutX(270.0);
+        label0.setLayoutY(344.0);
         label0.setText("PLAY OFFLINE");
         label0.setTextFill(javafx.scene.paint.Color.WHITE);
-        label0.setFont(new Font("Arial Bold", 13.0));
+        label0.setFont(new Font("Arial Bold", 15.0));
 
         label0.setEffect(dropShadow0);
 
-        label1.setLayoutX(408.0);
-        label1.setLayoutY(549.0);
+        label1.setLayoutX(271.0);
+        label1.setLayoutY(462.0);
         label1.setText("PLAY ONLINE");
         label1.setTextFill(javafx.scene.paint.Color.WHITE);
-        label1.setFont(new Font("Arial Bold", 13.0));
+        label1.setFont(new Font("Arial Bold", 15.0));
 
         label1.setEffect(dropShadow1);
 
-        anchorPane.getChildren().add(imageView);
+        getChildren().add(imageView);
         anchorPane.getChildren().add(imageView0);
+        anchorPane.getChildren().add(imageView1);
         anchorPane.getChildren().add(line);
         anchorPane.getChildren().add(line0);
         anchorPane.getChildren().add(line1);
         anchorPane.getChildren().add(line2);
         getChildren().add(anchorPane);
         getChildren().add(tictactoeLabel);
-        getChildren().add(imageView1);
+        getChildren().add(imageView2);
         getChildren().add(welcometoourtictactoeLabel);
         getChildren().add(historyButton);
         getChildren().add(signupButton);
@@ -278,7 +278,7 @@ public abstract class HomePage extends AnchorPane {
         getChildren().add(playvscomputerButton);
         getChildren().add(playofflineButton);
         getChildren().add(playonlineButton);
-        getChildren().add(imageView5);
+        getChildren().add(imageView6);
         getChildren().add(label);
         getChildren().add(label0);
         getChildren().add(label1);
