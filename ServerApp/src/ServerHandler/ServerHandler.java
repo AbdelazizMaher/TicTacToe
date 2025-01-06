8840024866647024cccfddd0b3d4886c1def0599
Adham Mohamed <adhambiko22@gmail.com>
2025-01-06 23:55:21 +0200

    Private.xml

--- ServerApp/src/ServerHandler/ServerHandler.java
+++ ServerApp/src/ServerHandler/ServerHandler.java
@@ -5,10 +5,32 @@
  */
 package ServerHandler;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Abdel
  */
-public class ServerHandler {
+public class ServerHandler extends Thread {
+        
+    ServerSocket serverSocket;
+    
+    public void startServer() {
+        try {
+            serverSocket = new ServerSocket(5005);
+            Socket clientSocket = serverSocket.accept();
+            new UserHandler(clientSocket);
+        } catch (IOException ex) {
+            Logger.getLogger(ServerHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void stopServer() {
+        
+    }
     
 }
