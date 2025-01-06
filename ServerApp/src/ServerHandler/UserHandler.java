4621377f7622666e92ffe9e1701a935dbe8bb716
Adham Mohamed <adhambiko22@gmail.com>
2025-01-06 15:42:21 +0200

    Create UserHandler Class

--- ServerApp/src/ServerHandler/UserHandler.java
+++ ServerApp/src/ServerHandler/UserHandler.java
@@ -5,10 +5,50 @@
  */
 package ServerHandler;
 
+import DataModels.ServerRequestInterface;
+
 /**
  *
  * @author Abdel
  */
-public class UserHandler {
+public class UserHandler implements ServerRequestInterface {
+
+    @Override
+    public void signUp() {}
+
+    @Override
+    public void signIn() {}
+
+    @Override
+    public void sendAvailablePlayers() {}
+
+    @Override
+    public void sendInvitation() {}
+
+    @Override
+    public void getInvetation() {}
+
+    @Override
+    public void sendMove() {}
+
+    @Override
+    public void gameWinner() {}
+
+    @Override
+    public void gameDraw() {}
+
+    @Override
+    public void withdraw() {}
+
+    @Override
+    public void logout() {}
+
+    @Override
+    public void connectionEnded() {}
     
+    
+    Thread th = new Thread(()->{
+        while(true)
+        {}
+    });
 }
