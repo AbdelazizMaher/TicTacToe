be61ea54fabfeaff040ff0e6f26cd59a8d694e22
Nermeen <nermeen41292@gmail.com>
2025-01-08 01:17:28 +0200

    Calling StartConnection from SignUp

--- ClientApp/src/XOControllers/SignupPageController.java
+++ ClientApp/src/XOControllers/SignupPageController.java
@@ -5,7 +5,9 @@
  */
 package XOControllers;
 
+import ClientHandler.ClientHandler;
 import XOGame.SignupPage;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
@@ -18,10 +20,33 @@ public class SignupPageController extends SignupPage {
 
     public SignupPageController(Stage stage) {
         registerButton.setOnAction(e -> {
-            Scene scene = new Scene(new HomePageController(stage));
-            stage.setScene(scene);
-        }
-        );
+            String name = usernameTextField.getText().trim();
+            String pass = passwordTextField.getText().trim();
+            String conf = confirmPasswordTextField.getText().trim();
+            String info = "signUp#@$"+name+"#@$"+pass+"#@$";
+                if(ClientHandler.startConnection(info)){               
+                    Thread thread = new Thread(()->{ 
+                        String message = ClientHandler.getResponse();
+                        if(message.equals("Signed Up")){
+                                Platform.runLater(()->{
+                                    Scene scene = new Scene(new HomePageController(stage));
+                                    stage.setScene(scene);
+                                });
+                            }
+                            else{
+                                Platform.runLater(()->{
+                                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                                    alert.setTitle("Server Error");
+                                    alert.setHeaderText(null);
+                                    alert.setContentText(message); 
+                                    alert.showAndWait();
+                                });                                 
+                            }
+                    });
+                    thread.setDaemon(true);
+                    thread.start();
+                }
+            });
 
         backButton.setOnMouseClicked(e -> {
             Scene scene = new Scene(new HomePageController(stage));
