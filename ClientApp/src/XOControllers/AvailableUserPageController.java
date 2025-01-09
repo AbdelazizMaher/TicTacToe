a39aab45f3cbdf295f46fad705f2560a21de2e9f
Nermeen <nermeen41292@gmail.com>
2025-01-09 02:21:08 +0200

    Update AvailableUserPageController to show the taken list from server

--- ClientApp/src/XOControllers/AvailableUserPageController.java
+++ ClientApp/src/XOControllers/AvailableUserPageController.java
@@ -7,18 +7,46 @@ package XOControllers;
 
 import XOGame.AvailableUsersPage;
 import XOGame.OnlinePage;
+import java.util.StringTokenizer;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 /**
  *
  * @author nerme
  */
 public class AvailableUserPageController extends AvailableUsersPage{
-    public AvailableUserPageController(Stage stage) {
+    public AvailableUserPageController(Stage stage,String online) {
+        rows.clear();
+        buttons.clear();
+        String[] playersArray = online.substring(1, online.length() - 1).split(", ");
+        Platform.runLater(()->{
+        for (String player : playersArray) {          
+            System.out.println(player);
+            if(!player.isEmpty()){
+                StringTokenizer info = new StringTokenizer(player, "#@$");
+                VBox row = new VBox(5);
+                Label playerName = new Label(info.nextToken());
+                playerName.setFont(new Font("Arial", 16));
+                Label playerScore = new Label(info.nextToken());
+                playerScore.setFont(new Font("Arial", 14));
+                Button inviteButton = new Button("Send Invitation");
+                inviteButton.setStyle("-fx-background-color: transparent; -fx-text-fill: red; -fx-font-size: 14px; -fx-underline: true;");
+                buttons.add(inviteButton);
+                row.getChildren().addAll(playerName, playerScore, inviteButton);
+                rows.add(row);
+            }
+        }
+        listView.getItems().addAll(rows);       
+        });
+        
         backButton.setOnMouseClicked(e -> {
             HomePageController root = new HomePageController(stage);
             Scene scene2 = new Scene(root);
