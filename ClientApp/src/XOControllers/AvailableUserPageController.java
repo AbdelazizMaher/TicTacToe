12c85eb56fc2216340f7ea66f890ddc1a0d9e177
iman <emanmhanafi@gmail.com>
2025-01-09 04:27:14 +0200

    send invitation from client

--- ClientApp/src/XOControllers/AvailableUserPageController.java
+++ ClientApp/src/XOControllers/AvailableUserPageController.java
@@ -5,6 +5,7 @@
  */
 package XOControllers;
 
+import ClientHandler.ClientHandler;
 import XOGame.AvailableUsersPage;
 import XOGame.OnlinePage;
 import javafx.scene.Scene;
@@ -18,17 +19,26 @@ import javafx.scene.control.ListView;
  * @author nerme
  */
 public class AvailableUserPageController extends AvailableUsersPage{
+    String messageRequest;
+    
     public AvailableUserPageController(Stage stage) {
         backButton.setOnMouseClicked(e -> {
             HomePageController root = new HomePageController(stage);
             Scene scene2 = new Scene(root);
             stage.setScene(scene2); 
         });
-        AvailableUsersPage availableUsersPage = new AvailableUsersPage();
-        Scene scene = new Scene(availableUsersPage);
-        stage.setScene(scene);
+        
+//        AvailableUsersPage availableUsersPage = new AvailableUsersPage();    //WHY ?????
+//        Scene scene = new Scene(availableUsersPage);
+//        stage.setScene(scene);
         for(Button b : buttons){
             b.setOnAction(e -> {
+//                messageRequest="send invitaion#@$"+"chico"+"#@$"+pass+"#@$";
+//                ClientHandler.sendRequest(text);
+                
+                
+                
+                
             Scene scene2 = new Scene(new OnlinePageController(stage));
             stage.setScene(scene2); 
             });                 
