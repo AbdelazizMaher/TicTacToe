4fb649a6c682657bdf4ea6507bb9b08fd5a5ec59
Nermeen <nermeen41292@gmail.com>
2025-01-10 05:27:22 +0200

    Change display as when the user is registered the logOut button appears

--- ClientApp/src/XOControllers/HomePageController.java
+++ ClientApp/src/XOControllers/HomePageController.java
@@ -19,41 +19,50 @@ import javafx.stage.Stage;
  * @author nerme
  */
 public class HomePageController extends HomePage {
-
     public HomePageController(Stage stage) {
-
-        signupButton.setOnAction(e -> {
-            Scene scene = new Scene(new SignupPageController(stage));
-            stage.setScene(scene);
-        });
-
-        loginButton.setOnAction(e -> {
-            Scene scene = new Scene(new LoginPageController(stage));
-            stage.setScene(scene);
-        });
-
-        historyButton.setOnAction(e -> {
-            Scene scene = new Scene(new HistoryPageController(stage));
-            stage.setScene(scene);
-        });
-
-        playvscomputerButton.setOnMouseClicked(e -> {
-            DifficultyLevelController root = new DifficultyLevelController(stage);
-            Scene scene2 = new Scene(root);
-            stage.setScene(scene2);
-        });
-
-        playofflineButton.setOnMouseClicked(e -> {
-            Scene scene2 = new Scene(new OfflinePageController(stage));
-            stage.setScene(scene2);
-            Platform.runLater(() -> {
-                new PopUpPageController(stage);
+        if(username.isEmpty()){
+            signupButton.setOnAction(e -> {
+                Scene scene = new Scene(new SignupPageController(stage));
+                stage.setScene(scene);
             });
-        });
 
-        playonlineButton.setOnMouseClicked(e -> {
-            PopUpRegisterController popup = new PopUpRegisterController(stage);
-            popup.show();
-        });
+            loginButton.setOnAction(e -> {
+                Scene scene = new Scene(new LoginPageController(stage));
+                stage.setScene(scene);
+            });
+
+            historyButton.setOnAction(e -> {
+                Scene scene = new Scene(new HistoryPageController(stage));
+                stage.setScene(scene);
+            });
+
+            playvscomputerButton.setOnMouseClicked(e -> {
+                DifficultyLevelController root = new DifficultyLevelController(stage);
+                Scene scene2 = new Scene(root);
+                stage.setScene(scene2);
+            });
+
+            playofflineButton.setOnMouseClicked(e -> {
+                Scene scene2 = new Scene(new OfflinePageController(stage));
+                stage.setScene(scene2);
+                Platform.runLater(() -> {
+                    new PopUpPageController(stage);
+                });
+            });
+
+            playonlineButton.setOnMouseClicked(e -> {
+                PopUpRegisterController popup = new PopUpRegisterController(stage);
+                popup.show();
+            });
+        }
+    
+        else{
+            
+            
+            playonlineButton.setOnMouseClicked(e -> {
+                Scene scene2 = new Scene(new AvailableUserPageController(stage));
+                stage.setScene(scene2);
+            });
+        }
     }
 }
