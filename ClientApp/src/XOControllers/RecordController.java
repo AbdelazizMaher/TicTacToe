00f0050c6ee1438129ebf59024d5b0e040b4143c
iman <emanmhanafi@gmail.com>
2025-01-13 23:30:32 +0200

    add createFile method; file name(player1 player2 time)

--- ClientApp/src/XOControllers/RecordController.java
+++ ClientApp/src/XOControllers/RecordController.java
@@ -5,8 +5,13 @@
  */
 package XOControllers;
 
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,8 +21,19 @@ public class RecordController {
     
     static String player1;
     static String player2;
+    static FileOutputStream fos;
+    static DataOutputStream dos;
    
-    
+      public static void createFile(){
+        String fileName = player1+"_"+player2+"_"+getCurrentTime();
+        try{     
+            fos = new FileOutputStream("../ClientApp/src/Record/" + fileName);        
+            dos = new DataOutputStream(fos);
+        } catch(IOException ex) {
+            Logger.getLogger(RecordController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+  
    
     private static String getCurrentTime() {
         LocalDateTime now = LocalDateTime.now();
