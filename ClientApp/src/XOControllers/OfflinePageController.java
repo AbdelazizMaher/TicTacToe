bf9ec503114b363955df370e6ff3812026b2df1b
ABDELAZIZ MAHER <abdelazizmaher17499@gmail.com>
2025-01-13 19:48:54 +0200

    modify access for som variables to use in controller

--- ClientApp/src/XOControllers/OfflinePageController.java
+++ ClientApp/src/XOControllers/OfflinePageController.java
@@ -7,25 +7,37 @@ package XOControllers;
 
 import XOGame.OfflinePage;
 import XOGameBoard.TicTacToe;
+import javafx.geometry.Bounds;
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 /**
  *
  * @author nerme
  */
-public class OfflinePageController extends OfflinePage{
+public class OfflinePageController extends OfflinePage {
+
     private boolean isPaused = false;
-    private TicTacToe XOgame;
-    
-    public OfflinePageController(Stage stage){
+    private TicTacToe xoGame;
+    private Line winningLine; 
+    public String player1;
+    public String player2;
+    private Stage stage;
+
+    public OfflinePageController(Stage stage) {
+        this.stage = stage;
+        xoGame = new TicTacToe();
+
         backButton.setOnMouseClicked(e -> {
             HomePageController root = new HomePageController(stage);
             Scene scene2 = new Scene(root);
-            stage.setScene(scene2); 
+            stage.setScene(scene2);
         });
         recordButton.setOnMouseClicked(e -> {
             Image recImage;
@@ -37,23 +49,58 @@ public class OfflinePageController extends OfflinePage{
             ImageView recImageView = new ImageView(recImage);
             recImageView.setFitHeight(40);
             recImageView.setFitWidth(40);
-            recordButton.setGraphic(recImageView);         
+            recordButton.setGraphic(recImageView);
             isPaused = !isPaused;
         });
+        
+        replayButton.setOnMouseClicked(e -> {
+            resetGame();
+        });
+
+        String[] players = xoGame.assignXOToPlayer();
+        player1 = players[0];
+        player2 = players[1];
+
+        initializeGameButtonsHandlers();
+
     }
-    
+
+    public void setOfflineToPlayerTurn() {
+        if (player1.equals("X")) {
+            playerOneLabel.setText(user1 + " turn");
+        } else {
+            playerOneLabel.setText(user2 + " turn");
+        }
+    }
+
+    private void initializeGameButtonsHandlers() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                final int rowButton = row;
+                final int colButton = col;
+                buttons[row][col].setOnAction(e -> {
+                    processMove(rowButton, colButton);
+                });
+            }
+        }
+    }
+
     private void processMove(int row, int col) {
+        if (xoGame.makeMove(row, col)) {
+            buttons[row][col].setText(xoGame.getCurrentPlayer());
+            if (xoGame.isWinningMove(row, col)) {
+                drawWinningLine();
+                updateScore();
+            } else if (xoGame.isDraw()) {
+            } else {
+                xoGame.switchPlayer();
+                setOfflineToPlayerTurn();
+            }
+        }
     }
-    
-    private void updateScore() {
-    
-    }
-    
-    private void resetGame() {
-    
-    }
-    
-    private void drawWinningLine() {
-    
-    }
+
+
+
+
+
 }
