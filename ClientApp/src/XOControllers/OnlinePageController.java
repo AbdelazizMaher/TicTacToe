e7a8cdeea79b32ee96782d0aab6edd7bb3334783
Adham Mohamed <adhambiko22@gmail.com>
2025-01-14 17:58:02 +0200

    Added ProcessMove & button handling in Online Mode

--- ClientApp/src/XOControllers/OnlinePageController.java
+++ ClientApp/src/XOControllers/OnlinePageController.java
@@ -5,21 +5,37 @@
  */
 package XOControllers;
 
+import ClientHandler.ClientHandler;
 import static XOGame.HomePage.userName;
 import XOGame.OnlinePage;
+import XOGameBoard.TicTacToe;
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 /**
  *
  * @author nerme
  */
-public class OnlinePageController extends OnlinePage{
-    private boolean isPaused = false; 
-    public OnlinePageController(Stage stage){
+public class OnlinePageController extends OnlinePage {
+
+    private boolean isPaused = false;
+    private TicTacToe xoGame;
+    private Line winningLine;
+    public static String userName;
+    public static String opponentName;
+    private Stage stage;
+
+    public OnlinePageController(Stage stage) {
+        this.stage = stage;
+        initializeGameButtonsHandlers();
         backButton.setOnMouseClicked(e -> {
+            ClientHandler.sendRequest("withdraw");
             AvailableUserPageController availablePage = new AvailableUserPageController(stage);
             Scene scene = new Scene(availablePage);
             stage.setScene(scene);
@@ -34,8 +50,45 @@ public class OnlinePageController extends OnlinePage{
             ImageView recImageView = new ImageView(recImage);
             recImageView.setFitHeight(40);
             recImageView.setFitWidth(40);
-            recordButton.setGraphic(recImageView);         
+            recordButton.setGraphic(recImageView);
             isPaused = !isPaused;
         });
+        replayButton.setOnMouseClicked(e -> {
+            ClientHandler.sendRequest("sendInvitaion" + "#@$" + userName + "#@$");
+        });
     }
+
+    private void initializeGameButtonsHandlers() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                final int rowButton = row;
+                final int colButton = col;
+                buttons[row][col].setStyle("-fx-font-size: 36px; -fx-font-weight: bold;");
+                buttons[row][col].setOnAction(e -> {
+                    processMove(rowButton, colButton);
+                });
+            }
+        }
+    }
+
+    private void processMove(int row, int col) {
+        if (xoGame.makeMove(row, col)) {
+
+            if (xoGame.isWinningMove(row, col) && winningLine == null) {
+                //1-send request with the winningmove & drawWinningLine();
+                ClientHandler.sendRequest("winningMove" + "#@$" + row + "#@$" + col + "#@$");
+                updateScore();
+                Scene scene = new Scene(new WinVideoPageController(stage));
+                stage.setScene(scene);
+            } else if (xoGame.isDraw()) {
+                //2-send request game is draw;
+                ClientHandler.sendRequest("drawMove" + "#@$" + row + "#@$" + col + "#@$");
+            } else {
+                //3-send request with the normalmove 
+                ClientHandler.sendRequest("normalMove" + "#@$" + row + "#@$" + col + "#@$");
+            }
+        }
+    }
+
+    
 }
