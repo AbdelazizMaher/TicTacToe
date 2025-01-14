41a72744a5e48c17261339eccf4f525a3bb5f5fc
ABDELAZIZ MAHER <abdelazizmaher17499@gmail.com>
2025-01-14 03:30:38 +0200

    merge in main

--- ClientApp/src/MainApp/ClientApp.java
+++ ClientApp/src/MainApp/ClientApp.java
@@ -5,23 +5,9 @@
  */
 package MainApp;
 
-<<<<<<< HEAD
-import ClientHandler.ClientHandler;
-import XOControllers.LoseVideoPageController;
-import XOGame.WinVideoPage;
-import XOControllers.WinVideoPageController;
+
 import XOControllers.HomePageController;
-import XOGame.HistoryPage;
-import XOGame.HomePage;
-import XOGame.LoginPage;
-import XOGame.OnlinePage;
-import XOGame.SignupPage;
-=======
-import XOGame.FXMLDocumentBase;
->>>>>>> main
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -35,12 +21,8 @@ public class ClientApp extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-<<<<<<< HEAD
         root = new HomePageController(stage);
-=======
-        Parent root = new FXMLDocumentBase();
-        
->>>>>>> main
+
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
